0b256166c2fc47ffcfeec41fa8d008f8e70abecd
Ordinastie <ordinastie@hotmail.com>
2016-10-07 14:21:51 +0200

    Added acceptedMinecraftVersions for 1.9.4

--- src/main/java/net/malisis/blocks/MalisisBlocks.java
+++ src/main/java/net/malisis/blocks/MalisisBlocks.java
@@ -46,7 +46,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
  * @author Ordinastie
  *
  */
-@Mod(modid = MalisisBlocks.modid, name = MalisisBlocks.modname, version = MalisisBlocks.version, dependencies = "required-after:malisiscore")
+@Mod(modid = MalisisBlocks.modid, name = MalisisBlocks.modname, version = MalisisBlocks.version, dependencies = "required-after:malisiscore", acceptedMinecraftVersions = "[1.9.4,1.10.2]")
 public class MalisisBlocks implements IMalisisMod
 {
 
